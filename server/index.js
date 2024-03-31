fc6870aba942c4d0f8ba2b84854a10302412dbeb
Vedveyaas <gshivamg8851@gmail.com>
2024-04-01 00:12:50 +0530

    client url added in server

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server,{
     cors:{
-        origin:["http://localhost:3000"],
+        origin:["https://6609adb066b233f458c1d034--realtime-with-chat-tic-tac-toe.netlify.app/"],
     }
 });
 
